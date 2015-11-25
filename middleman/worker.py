10b0ef26abb6d18d2aa0e3a5aab6198be7063871
Ben Scobie <admin@benscobie.com>
2015-11-25 21:04:18 +0000

    Working mysql and fetch

--- middleman/worker.py
+++ middleman/worker.py
@@ -5,18 +5,18 @@ class Worker:
     def __init__(self):
         self.db = None
 
-    def __del__(self):
-        self.db.close()
-
     def work(self):
-        self.db = _mysql.connect(host="localhost",user="root",passwd="",db="college_webaudit")
+        self.db = _mysql.connect(host="localhost",user="root",passwd="",db="webaudit")
         self.db.query("""SELECT * FROM scans WHERE scans.`status`= 0""")
 
         r = self.db.store_result()
 
-        for row in r.fetch_row(maxrows=0):
-            print(row[0])
+
+        for row in r.fetch_row(maxrows=0,how=1):
+            print(row["id"])
 
 
+	self.db.close()
 
-
+worker = Worker();
+worker.work();
