9e5cd871063e366110b19bf3db60892962ce982b
Ben Scobie <admin@benscobie.com>
2016-02-03 21:25:57 +0000

    Minor scanning process changes

--- middleman.py
+++ middleman.py
@@ -4,25 +4,30 @@ import time
 import threading
 from database import init_engine, db_session
 from models import Scan
-
+from datetime import datetime
 
 class Middleman:
 
     def __init__(self):
         self.config = configparser.ConfigParser()
         self.config.read('config.ini')
+        self.max_scans = int(self.config["WEBAUDIT"]["MaxConcurrentScans"])
 
         #init_engine('mysql+pymysql://' + self.config['DATABASE']['Username'] + ':' + self.config['DATABASE']['Password'] + '@' + self.config['DATABASE']['Server'] + '/' + self.config['DATABASE']['Database'], pool_recycle=3600)
         self.scans_in_progress = dict()
 
     def run(self):
         while True:
-            self.process_running_scans()
             self.process_queue()
-            time.sleep(5)
+            self.process_running_scans()
+            time.sleep(1)
 
     def process_running_scans(self):
-        print("Processing running scans", flush=True)
+        current_scans = len(self.scans_in_progress)
+        if current_scans == 0:
+            return
+
+        print("Processing running scans [" + str(current_scans) + "/" + str(self.max_scans) + "]" , flush=True)
         finished_scans = [];
 
         for key, list in self.scans_in_progress.items():
@@ -31,19 +36,26 @@ class Middleman:
 
         for finished_scan in finished_scans:
             scan = self.scans_in_progress[finished_scan]['scan']
+            scan.finished_date = datetime.utcnow()
             scan.status = 1
             db_session.commit()
-            del self.scans_in_progress[finished_scan]
+            self.scans_in_progress.pop(finished_scan, None)
 
     def process_queue(self):
         print("Processing queue", flush=True)
-        max_scans = int(self.config["WEBAUDIT"]["MaxConcurrentScans"])
         current_scans = len(self.scans_in_progress)
-        slots_available = (max_scans - current_scans)
+        slots_available = (self.max_scans - current_scans)
+
+        if current_scans > 0:
+            query = db_session.query(Scan).filter(Scan.status == 0).filter(~Scan.id.in_(self.scans_in_progress.keys())).order_by(Scan.created_date).limit(slots_available)
+        else:
+            query = db_session.query(Scan).filter(Scan.status == 0).order_by(Scan.created_date).limit(slots_available)
 
         if slots_available > 0:
-            for scan in db_session.query(Scan).filter(Scan.status == 0).filter(~Scan.id.in_(self.scans_in_progress.keys())).order_by(Scan.created_date).limit(slots_available):
+            for scan in query:
                 print("Scanning scan ID #" + str(scan.id), flush=True)
+                #scan.started_date = datetime.utcnow()
+                #db_session.commit()
                 scan_thread = threading.Thread(target=self.init_scan, args=(scan,))
                 scan_thread.start()
                 self.scans_in_progress[scan.id] = { 'scan': scan, 'thread': scan_thread }
--- tests/headers.py
+++ tests/headers.py
@@ -7,7 +7,14 @@ import requests
 class HeaderTest(WebTest):
     DATA_TYPE_HEADER = 1
 
-    match_headers = ['Server', 'X-Frame-Options', 'Content-Security-Policy', 'X-XSS-Protection', 'X-Content-Type-Options', 'Strict-Transport-Security']
+    match_headers = [
+        'Server',
+        'X-Frame-Options',
+        'Content-Security-Policy',
+        'X-XSS-Protection',
+        'X-Content-Type-Options',
+        'Strict-Transport-Security'
+    ]
 
     def __init__(self, scan):
         self.scan = scan
@@ -19,8 +26,6 @@ class HeaderTest(WebTest):
                 if header in self.match_headers:
                     header_scan_data = ScanData(scan_id=self.scan.id, data_type=self.DATA_TYPE_HEADER, key=header, value=r.headers[header])
                     db_session.add(header_scan_data)
-
-
             db_session.commit()
 
     def status(self):
